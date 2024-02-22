0e6780f8c7f01c84a1865dd5954e8f1f9862fcac
Gerry Mulvenna <git@movingwifi.com>
2024-02-22 17:02:03 +0000

    functions.php path

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@ session_start();
 //set Timezone
 date_default_timezone_set('Europe/London');
 
-require "../functions.php";
+require "functions.php";
 
 print head("some simple API interactions");
 
