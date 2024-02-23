1b1a0c8256939e4ed6668032f327ac6010f5a2c6
Gerry Mulvenna <git@movingwifi.com>
2024-02-23 17:05:59 +0000

    fix anchor

--- public_html/index.php
+++ public_html/index.php
@@ -9,13 +9,13 @@ require "functions.php";
 print head("some simple API interactions");
 
 ?>
-	<div class="card large">
+	<div class="card large alignleft">
 	<p>This is a showcase project to present some simple examples of API interactions, where the code (written in PHP) is deliberately 
 	kept relatively minimalist and self-contained, in order to best demonstrate the flow from Authentication to API call and response handling.</p>
 
 	<p>Gerry Mulvenna</p>
 
-	<a href="https://github.com/gerrymulvenna/apis.movingwifi.com" class="button">
+	<a href="https://github.com/gerrymulvenna/apis.movingwifi.com" class="button">View on GitHub</a>
 	</div>
 </div>
 </body>
