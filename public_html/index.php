72d69a880a863c62b1ba9b6daa1b6c74375dfdaf
Gerry Mulvenna <git@movingwifi.com>
2024-02-23 17:03:25 +0000

    front page blurb

--- public_html/index.php
+++ public_html/index.php
@@ -9,6 +9,14 @@ require "functions.php";
 print head("some simple API interactions");
 
 ?>
+	<div class="card large">
+	<p>This is a showcase project to present some simple examples of API interactions, where the code (written in PHP) is deliberately 
+	kept relatively minimalist and self-contained, in order to best demonstrate the flow from Authentication to API call and response handling.</p>
+
+	<p>Gerry Mulvenna</p>
+
+	<a href="https://github.com/gerrymulvenna/apis.movingwifi.com" class="button">
+	</div>
 </div>
 </body>
 </html>
