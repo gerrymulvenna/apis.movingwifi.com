2c112356937f9ac5089f2509021dfa654c7704f1
Gerry Mulvenna <git@movingwifi.com>
2024-02-23 17:13:40 +0000

    more blurb

--- public_html/index.php
+++ public_html/index.php
@@ -10,12 +10,26 @@ print head("some simple API interactions");
 
 ?>
 	<div class="card large">
-	<p class="alignleft">This is a showcase project to present some simple examples of API interactions, where the code (written in PHP) is deliberately 
-	kept relatively minimalist and self-contained, in order to best demonstrate the flow from Authentication to API call and response handling.</p>
+	<p class="alignleft">This is a showcase project to present some simple examples of API interactions, where the code (written in PHP) is kept deliberately 
+	minimalist and self-contained, in order to best demonstrate the flow from Authentication to API call and response handling. Each example
+	(accessed by the menu top right) implements the initial authenticated connection and present one or two examples of using the API.</p>
+	
+	<table><thead><tr><th>API</th><th>Example use</th></tr>
+	<tbody>
+		<tr>
+			<td>Google Calendar</td><td>Display your list of calendars<br>Display a list of future events from a calendar</td>
+		</tr>
+		<tr>
+			<td>Quickbooks</td><td>Display a summary table of invoices</td>
+		</tr>
+	</tbody>
+	</table>
+		
+			
 
 	<p class="alignleft">Gerry Mulvenna</p>
 
-	<a href="https://github.com/gerrymulvenna/apis.movingwifi.com" class="button">View on GitHub</a>
+	<a href="https://github.com/gerrymulvenna/apis.movingwifi.com" class="button tertiary">View the code on GitHub</a>
 	</div>
 </div>
 </body>
