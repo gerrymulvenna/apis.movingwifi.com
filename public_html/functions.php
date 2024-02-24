44a980eb117a9e64df6f6827f922a104d2026d89
Gerry Mulvenna <git@movingwifi.com>
2024-02-24 13:34:31 +0000

    xero added to menu

--- public_html/functions.php
+++ public_html/functions.php
@@ -62,6 +62,7 @@ function head($title, $home = "", $subtitle = "simple API interaction")
 				<a href="/">Start page</a> 
 				<a href="/google/">Google Calendar</a> 
 				<a href="/quickbooks/">Quickbooks</a>
+				<a href="/xero/">Xero</a>
 				<a href="https://github.com/gerrymulvenna/apis.movingwifi.com">View code on GitHub</a>
 			</nav>
 		</div>
