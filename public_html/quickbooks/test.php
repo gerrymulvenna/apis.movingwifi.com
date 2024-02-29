4777296e234b3753c2c3735d87a85c1eb632975e
Gerry Mulvenna <git@movingwifi.com>
2024-02-29 11:40:46 +0000

    test cookie name

--- public_html/quickbooks/test.php
+++ public_html/quickbooks/test.php
@@ -4,14 +4,11 @@ error_reporting(-1);
 session_start();  //use session cookie for state 
 //set Timezone
 date_default_timezone_set('Europe/London');
+$cookie = "movingwifi_Quickbooks";
 
-setcookie('gerry',serialize($_COOKIE),time()+3600, "/");
+setcookie($cookie,serialize($_COOKIE),time()+3600, "/");
 
 print "<pre>\n";
 print_r($_COOKIE);
-print "\n";
-$t = strtotime('+6 months');
-print "$t\n";
-print date("c",$t);
 print "</pre>\n";
 ?>
