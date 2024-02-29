41cd8d4bc8bb07f5e0a1e93cfbc04f1a7e515028
Gerry Mulvenna <git@movingwifi.com>
2024-02-29 15:21:30 +0000

    some cookie / no cookie logic in test script

--- public_html/quickbooks/test.php
+++ public_html/quickbooks/test.php
@@ -7,7 +7,36 @@ date_default_timezone_set('Europe/London');
 
 require "../functions.php";
 
-$cookie = "movingwifi_Quickbooks";
+$title = "Test PHP script";
+$cookie = "movingwifi_test";
+
+if (isset($_COOKIE[$cookie]))
+{
+	if (isset($_REQUEST['operation']))
+	{
+		if ($_REQUEST['operation'] == 'revoke')
+		{
+			setcookie($cookie, "", time()-3600, "/");
+			print head($title, "Revoked");
+			print "</div></body></html>\n";
+		}
+	}
+	else
+	{
+		$num = $_COOKIE[$cookie];
+		setcookie($cookie, $num + 1,time()+3600, "/");
+		print head($title, sprintf("%04d", $num));
+		print generic_button("revoke","Revoke",['operation'=>'revoke'],"tertiary",'GET','./test.php');
+		print "</div></body></html>\n";
+	}
+}
+else
+{
+	setcookie($cookie, 1, time()+3600, "/");
+	print head($title,"Home"));
+	print generic_button("revoke","Revoke",['operation'=>'revoke'],"tertiary",'GET','./test.php');
+	print "</div></body></html>\n";
+	
 
 setcookie($cookie,serialize($_COOKIE),time()+3600, "/");
 print head("Test PHP script","Home");
