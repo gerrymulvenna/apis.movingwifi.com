ef5bca0874758640b0bb8c30f1af5cbd2630d60b
Gerry Mulvenna <git@movingwifi.com>
2024-02-29 11:53:24 +0000

    head() in test script

--- public_html/quickbooks/test.php
+++ public_html/quickbooks/test.php
@@ -4,10 +4,13 @@ error_reporting(-1);
 session_start();  //use session cookie for state 
 //set Timezone
 date_default_timezone_set('Europe/London');
+
+require "../functions.php";
+
 $cookie = "movingwifi_Quickbooks";
 
 setcookie($cookie,serialize($_COOKIE),time()+3600, "/");
-
+print head("Test PHP script","Home");
 print "<pre>\n";
 print_r($_COOKIE);
 print "</pre>\n";
