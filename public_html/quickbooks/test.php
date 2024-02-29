70f8e6dd02edc3245866fd97de8a0c5a40c0fa50
Gerry Mulvenna <git@movingwifi.com>
2024-02-29 15:28:55 +0000

    test.php restart, revoke, increment buttons

--- public_html/quickbooks/test.php
+++ public_html/quickbooks/test.php
@@ -17,7 +17,8 @@ if (isset($_COOKIE[$cookie]))
 		if ($_REQUEST['operation'] == 'revoke')
 		{
 			setcookie($cookie, "", time()-3600, "/");
-			print head($title, "Revoked");
+			print head($title . " REVOKED", "");
+			print generic_button("restart","Restart",[],"tertiary",'GET','./test.php');
 			print "</div></body></html>\n";
 		}
 	}
@@ -25,7 +26,8 @@ if (isset($_COOKIE[$cookie]))
 	{
 		$num = $_COOKIE[$cookie];
 		setcookie($cookie, $num + 1,time()+3600, "/");
-		print head($title, sprintf("%04d", $num));
+		print head($title.sprintf(" %04d", $num), "");
+		print generic_button("increment","Increment",['operation'=>'increment'],"tertiary",'GET','./test.php');
 		print generic_button("revoke","Revoke",['operation'=>'revoke'],"tertiary",'GET','./test.php');
 		print "</div></body></html>\n";
 	}
@@ -33,7 +35,8 @@ if (isset($_COOKIE[$cookie]))
 else
 {
 	setcookie($cookie, 1, time()+3600, "/");
-	print head($title,"Home");
+	print head($title . " START","");
+	print generic_button("increment","Increment",['operation'=>'increment'],"tertiary",'GET','./test.php');
 	print generic_button("revoke","Revoke",['operation'=>'revoke'],"tertiary",'GET','./test.php');
 	print "</div></body></html>\n";
 }
