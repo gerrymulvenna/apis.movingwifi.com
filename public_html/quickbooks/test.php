53bc99b470ebb8f5d731cb8658209cb6029dd238
Gerry Mulvenna <git@movingwifi.com>
2024-03-01 10:01:11 +0000

    use radio shows api for more complex test

--- public_html/quickbooks/test.php
+++ public_html/quickbooks/test.php
@@ -9,6 +9,7 @@ require "../functions.php";
 
 $title = "Test PHP script";
 $cookie = "movingwifi_test";
+$url = "https://charts.indylive.radio/showjson.php";
 
 if (isset($_COOKIE[$cookie]))
 {
@@ -46,11 +47,75 @@ if (isset($_COOKIE[$cookie]))
 }
 else
 {
-	setcookie($cookie, 1, time()+3600, "/");
-	print head($title . " START","");
-	print generic_button("home","Home",[],"primary",'GET','./test.php');
-	print generic_button("increment","Increment",['operation'=>'increment'],"tertiary",'GET','./test.php');
-	print generic_button("revoke","Revoke",['operation'=>'revoke'],"tertiary",'GET','./test.php');
-	print "</div></body></html>\n";
+	$data = apiTest($url);
+	if ($data['code'] == 200)
+	{
+		$shows = $data['response'];
+		setcookie($cookie, serialize($shows), strtotime('+6 months'), "/");
+		print head($title, "Connected - click to continue", count(shows));
+		print "</div></body></html>\n";
+	}
+	else
+	{
+		print head($title, "Error retrieving shows", ""));
+		print "</div></body></html>\n";
+	}
 }
+
+
+/**
+ * sends an API call using GET method and without authentication
+ *
+ * @param string $url destination address
+ * @param array $vars Associative array of variables to send with the request
+ */
+function apiTest($url, $method = 'GET', $vars = [], $headers = [])
+{
+	// add required headers
+	array_push($headers, 'Accept: application/json');
+	array_push($headers, 'Content-Type: application/json');
+
+    // Set up cURL options.
+    $ch = curl_init();
+	curl_setopt($ch, CURLOPT_VERBOSE, true);
+	$eh = fopen('curl.log', 'w+');
+	curl_setopt($ch, CURLOPT_STDERR, $eh);
+	if ($method == 'GET' && count($vars)>0)
+	{
+		$query = http_build_query($vars);
+		if (strpos($url, '?'))
+		{
+			$url .= '&' . $query;
+		}
+		else
+		{
+			$url .= '?' . $query;
+		}
+	}
+		
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, "MOVINGWIFI_PHP/1.0");
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	if ($method == 'POST')
+	{
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($vars));
+	}
+	
+    $response = curl_exec($ch);
+    $error = curl_error($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    // Set the header, response, error and http code.
+	$data = [];
+    $data['response'] = json_decode($response);
+    $data['error'] = $error;
+    $data['code'] = $http_code;
+	return $data;
+}
+
+
 ?>
