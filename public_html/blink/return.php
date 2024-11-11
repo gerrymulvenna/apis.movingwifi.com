a954c73b73cfc5d4dee0c6875f1549618cb8adf3
Gerry Mulvenna <git@movingwifi.com>
2024-11-11 12:11:13 +0000

    return.php improved, align right, mdata json

--- public_html/blink/return.php
+++ public_html/blink/return.php
@@ -10,13 +10,13 @@ print head($title, "Click to continue", "Payment response");
 $id = $_GET["transaction_id"];
 $note = $_GET["note"];
 $status = $_GET["status"];
-$mdata = json_decode($_GET["merchant_data"]);
+$mdata = $_GET["merchant_data"];
 
-print "<table>
-<tr><td>transaction_id</td><td>$id</td></tr>
-<tr><td>note</td><td>$note</td></tr>
-<tr><td>status</td><td>$status</td></tr>
-<tr><td>merchant_data</td><td>" . print_r($mdata) . "</td></tr>
+print "<table style=\"width:fit-content;\">
+<tr><td style=\"text-align:right;\">transaction_id</td><td>$id</td></tr>
+<tr><td style=\"text-align:right;\">note</td><td>$note</td></tr>
+<tr><td style=\"text-align:right;\">status</td><td>$status</td></tr>
+<tr><td style=\"text-align:right;\">merchant_data</td><td>" . $mdata . "</td></tr>
 </table>\n";
 
 ?>
