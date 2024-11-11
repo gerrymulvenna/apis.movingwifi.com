2f60197987d331066f71be8bbde19b8c1a858c5c
Gerry Mulvenna <git@movingwifi.com>
2024-11-11 12:47:56 +0000

    left align data points, urldecode note

--- public_html/blink/return.php
+++ public_html/blink/return.php
@@ -8,19 +8,19 @@ $title = "Blink payment sandbox";
 
 print head($title, "Click to continue", "Payment response");
 $id = $_GET["transaction_id"];
-$note = $_GET["note"];
+$note = urldecode($_GET["note"]);
 $status = $_GET["status"];
+$mdata = json_decode(urldecode($_GET["Merchant data"]));
 
 print "<div class=\"card large warning\">
 <h3 class=\"section\">Transaction</h3>
-<p class=\"section\">
+<p class=\"section\" style=\"text-align: left;\">
 <strong>transaction_id:</strong> $id<br/>
 <strong>note:</strong> $note<br/>
 <strong>status:</strong> $status</p>
 </div>\n";
 
-$mdata = json_decode(urldecode($_GET["merchant_data"]));
-$html = "<div class=\"card large error\"><h3 class=\"section\">merchant_data</h3>\n<p class=\"section\">";
+$html = "<div class=\"card large error\"><h3 class=\"section\">merchant_data</h3>\n<p class=\"section\" style=\"text-align:left;\">";
 foreach (get_object_vars($mdata) as $key => $value)
 {
 	$html .= "<strong>$key:</strong> $value<br/>\n";
