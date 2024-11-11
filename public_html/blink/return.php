0c81b6788c3f14afa50f98e683f83ea2ee31576b
Gerry Mulvenna <git@movingwifi.com>
2024-11-11 12:15:02 +0000

    decode merchant data

--- public_html/blink/return.php
+++ public_html/blink/return.php
@@ -10,13 +10,13 @@ print head($title, "Click to continue", "Payment response");
 $id = $_GET["transaction_id"];
 $note = $_GET["note"];
 $status = $_GET["status"];
-$mdata = $_GET["merchant_data"];
+$mdata = json_decode(urldecode($_GET["merchant_data"]));
 
 print "<table style=\"width:fit-content;\">
 <tr><td style=\"text-align:right;\">transaction_id</td><td>$id</td></tr>
 <tr><td style=\"text-align:right;\">note</td><td>$note</td></tr>
 <tr><td style=\"text-align:right;\">status</td><td>$status</td></tr>
-<tr><td style=\"text-align:right;\">merchant_data</td><td>" . $mdata . "</td></tr>
+<tr><td style=\"text-align:right;\">merchant_data</td><td>" . print_r($mdata) . "</td></tr>
 </table>\n";
 
 ?>
