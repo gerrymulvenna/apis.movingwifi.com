8cc2a7f7ce950d97510bf9358d02315fd3761134
Gerry Mulvenna <git@movingwifi.com>
2024-11-11 12:07:56 +0000

    return.php improved

--- public_html/blink/return.php
+++ public_html/blink/return.php
@@ -3,7 +3,20 @@
 error_reporting(-1);
 //set Timezone
 date_default_timezone_set('Europe/London');
+require "../functions.php";
+$title = "Blink payment sandbox";
 
-print json_encode($_REQUEST);
+print head($title, "Click to continue", "Payment response");
+$id = $_GET["transaction_id"];
+$note = $_GET["note"];
+$status = $_GET["status"];
+$mdata = json_decode($_GET["merchant_data"]);
+
+print "<table>
+<tr><td>transaction_id</td><td>$id</td></tr>
+<tr><td>note</td><td>$note</td></tr>
+<tr><td>status</td><td>$status</td></tr>
+<tr><td>merchant_data</td><td>" . print_r($mdata) . "</td></tr>
+</table>\n";
 
 ?>
